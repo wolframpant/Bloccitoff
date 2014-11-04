7bc4b448171f6a967699ef4adc23272be8566e17
Ellen W <llnwlfsn@gmail.com>
2014-11-04 22:07:18 +0000

    specs passing

--- spec/features/project_manager_creates_todo_spec.rb
+++ spec/features/project_manager_creates_todo_spec.rb
@@ -2,18 +2,31 @@ require 'rails_helper'
 
 feature 'Project manager creates a TODO' do
   scenario 'Successfully' do
+    user = create(:user, email: "abc@example.com")
+    visit new_user_session_path
+    fill_in 'Email', with: 'abc@example.com'
+    fill_in 'Password', with: 'helloworld'
+    click_button('Log in') 
+    expect(page).to have_content 'Signed in successfully.' 
     visit new_todo_path
     fill_in 'Description', with: 'Meet up with the team'
     click_button 'Save'
     expect(page).to have_content('Your new TODO was saved')
     expect(page).to have_content('Meet up with the team')
   end
-
- feature 'With description missing'
-   scenario 'Successfully' do
-     visit new_todo_path
-     click_button 'Save'
-     expect(page).to have_content('You forgot to enter a description')
-   end
-
 end
+
+feature 'With description missing' do
+  scenario 'Unsuccessfully' do
+    user = create(:user, email: "abc@example.com")
+    visit new_user_session_path
+    fill_in 'Email', with: 'abc@example.com'
+    fill_in 'Password', with: 'helloworld'
+    click_button('Log in') 
+    expect(page).to have_content 'Signed in successfully.' 
+    visit new_todo_path
+    click_button 'Save'
+    expect(page).to have_content('You forgot to enter a description')
+  end
+end
+
